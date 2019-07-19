aff1679974fa197b5e283451d923e3c44d2c60e6
John Kim <john.kim@dedhamgroup.com>
2019-07-19 14:10:56 -0400

    Result

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           Check against pushed branch
         </p>
         <p>
-          Await! Log it. Its working? I think so! Holup. test he result.
+          Await! Log it. Its working? I think so! Holup. test the result.
         </p>
         <a
           className="App-link"
