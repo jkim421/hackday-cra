fd325f5fde270894b2541a1fdbdc74f3f89662c7
John Kim <john.kim@dedhamgroup.com>
2019-07-19 13:48:27 -0400

    Destructured request.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           Comparison working?
         </p>
         <p>
-          Log the comparison.
+          Log the comparison. Destructurd.
         </p>
         <a
           className="App-link"
